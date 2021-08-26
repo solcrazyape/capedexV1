c6f52b610a0ae9f8e5f54e63fb3d65e5f630c7a6
Crazy Ape Coin <85198323+solcrazyape@users.noreply.github.com>
2021-08-26 16:27:50 -0700

    Initial commit

---
+++ src/components/HelpUrls.ts
@@ -0,0 +1,12 @@
+export const helpUrls = {
+  website: 'https://crazyapecoin.com',
+  customerSupport: 'https://t.me/solcrazyape',
+  contactEmail: 'hello@crazyapecoin.com',
+  discord: 'https://discord.gg/Uj2ZMAXfpC',
+  telegram: 'https://t.me/solcrazyape',
+  twitter: 'https://twitter.com/solcrazyape',
+  github: 'https://github.com/solcrazyape',
+  projectSerum: 'https://projectserum.com/',
+  developerResources: 'https://projectserum.com/developer-resources',
+  solanaBeach: 'https://solanabeach.io/token/GpYMp8eP3HADY8x1jLVfFVBVYqxFNxT5mFhZAZt9Poco',
+};
